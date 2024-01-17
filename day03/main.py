f3729241abe6503227d7eb87fa6eb2de3b51c8e5
Christian Drumm <christian.drumm@hey.com>
2024-01-17 21:57:39 +0100

    solution 2016 day 3

---
+++ day03/main.py
@@ -0,0 +1,29 @@
+from commons import read_input_file
+
+
+def is_possible(triangle: list[int]) -> bool:
+    a, b, c = triangle
+    return a + b > c and a + c > b and c + b > a
+
+
+def solve_part_1(
+    lines: list[str],
+) -> int:
+    triangles = [list(map(int, line.split())) for line in lines]
+    possible = [t for t in triangles if is_possible(t)]
+    return len(possible)
+
+
+def solve_part_2(
+    lines: list[str],
+) -> int:
+    input = [list(map(int, line.split())) for line in lines]
+    values = [v[0] for v in input] + [v[1] for v in input] + [v[2] for v in input]
+    triangles = [values[i : i + 3] for i in range(0, len(values), 3)]
+    possible = [t for t in triangles if is_possible(t)]
+    return len(possible)
+
+
+if __name__ == "__main__":
+    print(solve_part_1(read_input_file("input.txt")), "of the triangles are possible.")
+    print(solve_part_2(read_input_file("input.txt")), "of the triangles are possible.")
